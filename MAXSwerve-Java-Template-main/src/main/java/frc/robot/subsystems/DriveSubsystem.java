1a6e985d6a4c9849213e8b0fae8a852e96ddb14b
Scallywag <79471311+onaysphere@users.noreply.github.com>
2024-01-17 21:50:51 -0500

    Swerve fix????????????????

--- MAXSwerve-Java-Template-main/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ MAXSwerve-Java-Template-main/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -14,6 +14,8 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import frc.robot.Constants.DriveConstants;
 import frc.utils.SwerveUtils;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -41,7 +43,7 @@ public class DriveSubsystem extends SubsystemBase {
       DriveConstants.kBackRightChassisAngularOffset);
 
   // The gyro sensor
-  private final ADIS16470_IMU m_gyro = new ADIS16470_IMU();
+  private final WPI_Pigeon2 m_gyro = new WPI_Pigeon2(7);
 
   // Slew rate filter variables for controlling lateral acceleration
   private double m_currentRotation = 0.0;
